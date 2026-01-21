5a81fa52af581f3d877bc4865072146631b7b7fb
lokki <lokki@linux.lokki>
2026-01-21 10:55:39 -0300

    finalizando cadastro de usuarios

--- Main.java
+++ Main.java
@@ -8,7 +8,7 @@ public class Main {
 
 
         String menu = """
-        =====================
+        \n=====================
         1 - Cadastrar usuario
         2 - Listar usuarios
         3 - Remover usuarios
@@ -23,6 +23,33 @@ public class Main {
             System.out.print("Digite sua opção: ");
             opcao = inputt.nextInt();
 
+            switch (opcao){
+                case 1:
+                    System.out.print("\nDigite seu nome: ");
+                    inputt.nextLine(); //limpar buffer
+                    String nome = inputt.nextLine();
+                    System.out.print("Digite sua idade: ");
+                    int idade = inputt.nextInt();
+                    inputt.nextLine(); //limpar buffer
+                    System.out.print("Digite seu email: ");
+                    String email = inputt.nextLine();
+
+                    //cria o usuario
+                    Usuario novoUsuario = new Usuario(nome, idade, email);
+
+                    //adiciona na lista
+                    usuarios.add(novoUsuario);
+
+                    System.out.println("\nCadastrado com sucesso\n");
+
+
+
+                    break;
+
+                case 2:
+                    System.out.println("finalizando...");
+                    break;
+            }
 
         }
 
