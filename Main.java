8ee3dfd06af15868116b278ca550f477f43cb11f
lokki <lokki@linux.lokki>
2026-01-22 10:16:26 -0300

    adicionado listar e remover

--- Main.java
+++ Main.java
@@ -12,7 +12,7 @@ public class Main {
         1 - Cadastrar usuario
         2 - Listar usuarios
         3 - Remover usuarios
-        0 - Sair        
+        0 - Sair
         =====================
         """;
 
@@ -23,7 +23,7 @@ public class Main {
             System.out.print("Digite sua opção: ");
             opcao = inputt.nextInt();
 
-            switch (opcao){
+            switch (opcao) {
                 case 1:
                     System.out.print("\nDigite seu nome: ");
                     inputt.nextLine(); //limpar buffer
@@ -43,15 +43,59 @@ public class Main {
                     System.out.println("\nCadastrado com sucesso\n");
 
 
-
                     break;
 
                 case 2:
-                    System.out.println("finalizando...");
+                    if (usuarios.isEmpty()) {
+                        System.out.println("Sem usuarios cadastrados ainda");
+
+                    } else {
+                        for (int i = 0; i < usuarios.size(); i++) {
+                            Usuario u = usuarios.get(i);
+                            System.out.println("\nUSUARIO: " + (i + 1));
+                            System.out.println("NOME: " + u.nome);
+                            System.out.println("IDADE: " + u.idade);
+                            System.out.println("EMAIL: " + u.email);
+
+                        }
+                    }
                     break;
+
+
+                case 3:
+                    if (usuarios.isEmpty()){
+                        System.out.println("Sem usuarios cadastrados ainda");
+
+                    } else {
+                        System.out.print("Qual usuario deseja remover?");
+
+                        for (int i = 0; i < usuarios.size(); i++) {
+                            Usuario u = usuarios.get(i);
+                            System.out.println("\nUSUARIO: " + (i + 1));
+                            System.out.println("NOME: " + u.nome);
+                            System.out.println("IDADE: " + u.idade);
+                            System.out.println("EMAIL: " + u.email);
+                        }
+
+
+                        System.out.println("\nDigite o número do usuario que será removido: ");
+                            int opc = inputt.nextInt();
+                            opc = opc - 1;
+
+                        if (opc >= 0 && opc < usuarios.size()) {
+                            usuarios.remove(opc);
+                            System.out.println("Removido com sucesso\n");
+
+                        } else {
+                            System.out.println("\nUsuario não encontrado\n");
+                        }
+                    }
+                    break;
+
+            }
+
             }
 
         }
 
     }
-}
