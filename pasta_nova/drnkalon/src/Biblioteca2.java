1d0533c8aef8ba8ff06340427ed101e3457b21f0
Moises-Sousa0 <moisessousanow@gmail.com>
2026-08-16 01:52:19 -0300

    aprendendo final, set/getters/ controladores de acesso, construtor e etc

---
+++ pasta_nova/drnkalon/src/Biblioteca2.java
@@ -0,0 +1,10 @@
+public class Biblioteca2 {
+    public static void main(String[] args) {
+        Livro2 livro1 = new Livro2("loki", "lokizada", 1960);
+        Livro2 livro21 = new Livro2("kanye west", "kanye", 1920);
+        Livro2 livro3 = new Livro2("Father", "sla", 1922);
+        System.out.println(Livro2.totalLivros);
+
+    }
+
+}
---
+++ pasta_nova/drnkalon/src/Livro2.java
@@ -0,0 +1,59 @@
+public class Livro2 {
+    private String titulo;
+    private String autor;
+    private final int anoPublicado;
+    private boolean disponivel;
+    static int totalLivros;
+
+    Livro2(String titulo, String autor, int anoPublicado){
+        this.titulo = titulo;
+        this.autor =  autor;
+        if(anoPublicado <= 0){
+            System.out.println("Ano inválido");
+            this.anoPublicado = 0;
+        } else {
+            this.anoPublicado = anoPublicado;
+        }
+        totalLivros++;
+    }
+
+    public String getTitulo() {
+        return this.titulo;
+    }
+
+    public void setTitulo(String titulo){
+        if (titulo == null || titulo.isEmpty()){
+            System.out.println("Valor inválido!");
+        }else {
+            this.titulo = titulo;
+        }
+
+    }
+
+    public String getAutor(){
+        return this.autor;
+    }
+
+    public void setAutor(String autor){
+        if (autor == null || autor.isEmpty()){
+            System.out.println("Valor inválido!");
+        } else{
+            this.autor = autor;
+        }
+
+    }
+
+    public int getAnoPublicado(){
+        return this.anoPublicado;
+    }
+
+
+
+    public boolean getDisponivel(){
+        return this.disponivel;
+    }
+
+    public void isDisponivel(boolean disponivel){
+        this.disponivel = disponivel;
+    }
+}
