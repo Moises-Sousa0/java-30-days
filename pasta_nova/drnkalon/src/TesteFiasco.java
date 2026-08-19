3009c2bb0af6047433dcc9cbaab73917c2a5a3de
Moises-Sousa0 <moisessousanow@gmail.com>
2026-08-18 23:46:05 -0300

    teste arrays

---
+++ pasta_nova/drnkalon/src/TesteFiasco.java
@@ -0,0 +1,10 @@
+public class TesteFiasco {
+    public static void main(String[] args) {
+        String[] ninja = new String[3];
+        ninja[0] = "teste1";
+        ninja[1] = "teste2";
+        ninja[2] = "teste3";
+
+        System.out.println(ninja[2]);
+    }
+}
