d4dd8d52caa5dc905eb0b275cf1633b52a7e0605
Moises-Sousa0 <moisessousanow@gmail.com>
2026-08-19 02:06:11 -0300

    array basicao

---
+++ pasta_nova/drnkalon/src/MainGenerics.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class MainGenerics {
+    public static void main(String[] args) {
+        String[] pecas = new String[3];
+         pecas[0] = "Oleo";
+         pecas[1] = "Pastilha";
+         pecas[2] = "Filtro";
+
+        System.out.println(pecas[0]);
+        System.out.println(pecas[1]);
+        System.out.println(pecas[2]);
+
+    }
+}
